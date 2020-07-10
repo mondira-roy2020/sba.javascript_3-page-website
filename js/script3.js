18409c5551a90d2fb5374b7078ce4ecfe5bed6ed
mondira-roy2020 <mondira.roy.git2020@gmail.com>
2020-07-10 14:47:31 -0400

    nearly done

---
+++ js/script3.js
@@ -0,0 +1,22 @@
+const nextBtn=document.querySelector('.nextBtn');
+const prevBtn=document.querySelector('.prevBtn');
+const container=document.querySelector('.images');
+
+let counter=0;
+
+nextBtn.addEventListener('click',nextPic);
+
+prevBtn.addEventListener('click',prevPic);
+
+function nextPic() {
+
+    if (counter === 10){
+        counter= -1;
+    }
+    counter++;
+    container.style.backgroundImage = `url(/img/president-${counter}.jpg)`
+}
+
+function prevPic(){
+
+}
